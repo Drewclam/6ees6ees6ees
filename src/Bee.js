740b45fd57bf966f956f313c70100c9b4eccbba9
Drewclam <andrew.c.lam94@gmail.com>
2017-04-14 12:08:03 -0400

    Complete all specs

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,7 @@
-class Bee {
-  // TODO..
+class Bee extends Grub{
+  constructor() {
+    super();
+    this.age = 5;
+    this.job = 'Keep on growing';
+  }
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,10 @@
 class Grub {
+  constructor() {
+    this.age = 5;
+    this.color = 'yellow';
+    this.food = 'jelly';
+    this.eat = function() {
+      return 'Mmmmmmmmm jelly';
+    }
+  }
 };
